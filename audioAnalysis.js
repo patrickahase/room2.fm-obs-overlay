00ff334c31cb9964ae7267cfc3a7ffcf82dff615
Hase <patrickahase@gmail.com>
2022-09-25 09:57:33 +1000

    audio analysis update

--- audioAnalysis.js
+++ audioAnalysis.js
@@ -4,32 +4,32 @@ function setup() {
   createCanvas(0, 0);
   stereoIn = new p5.AudioIn();
   stereoIn.start();
-  fft = new p5.FFT();
+  fft = new p5.FFT(0.989);
   fft.setInput(stereoIn);
 }
 
 function draw() {
-  let spectrum = fft.analyze();
+  let spectrum = fft.analyze(256);
   // example of one frequency analysis band
   // see the getEnergy documentation for more info on what you can
   // define the band as - but it returns a range of 0 - 255
-  let mid = fft.getEnergy("mid");
   // the remap function takes in the fft value and maps it to a range
   // defined by the second two numbers. uncomment the line below to 
   // see in the console an example of remaping the mid to a 0 - 100 range
   // console.log(remapEnergy(mid, 0, 100));
 
   // below i've implemented bass/mid/hi to the three phase axes - hopefully makes sense
-  let bass = fft.getEnergy("bass");
-  let treble = fft.getEnergy("treble");
-  document.documentElement.style.setProperty("--font-var-one", remapEnergy(bass, 0, 100));
-  document.documentElement.style.setProperty("--font-var-two", remapEnergy(mid, 0, 100));
-  document.documentElement.style.setProperty("--font-var-three", remapEnergy(treble, 0, 100));
+  let mid = remapEnergy(fft.getEnergy(50,220),150,175);
+  let treble = remapEnergy(fft.getEnergy(220,14000),14,26);
+
+  document.documentElement.style.setProperty("--font-var-two", mid);
+  document.documentElement.style.setProperty("--font-var-three", treble);
 }
 
 function remapEnergy(energyInput, floor, ceiling){
   // this expects the 0 - 255 range of the getEnergy
-  return floor + (ceiling - floor) * energyInput/255;
+  // return floor + (ceiling - floor) * energyInput/255;
+  return (energyInput - floor)/(ceiling - floor) * 100;
 }
 
 // get latest prompt from server
